e24cda1e625b8ee33a2880810ddf93553a0c9cae
Pavel_Ananich <pavel_ananich@epam.com>
2019-03-13 17:24:39 +0300

    Initial commit

---
+++ src/main/java/ananich/projects/App.java
@@ -0,0 +1,13 @@
+package ananich.projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
